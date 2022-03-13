4b97a9f8ba363c233fdda763044e54b0fcf8e11c
Deco <gdecosmojr@gmail.com>
2022-03-12 20:29:02 -0500

    change location of power switch

---
+++ raspinet/bin/power_switch.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Author: Gary Decosmo
+# Description: service for power_switch
+
+
+### BEGIN INIT INFO
+# Provides:          powerSwitch.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+# If you want a command to always run, put it here
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting powerswitch.py"
+    /usr/local/bin/power-switch.py &
+    ;;
+  stop)
+    echo "Stopping power-switch.py"
+    pkill -f /usr/local/bin/power-switch.py
+    ;;
+  *)
+    echo "Usage: /etc/init.d/power-switch.sh {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
