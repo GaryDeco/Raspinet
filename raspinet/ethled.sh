46bca6719ee0a7928c5e41f7f93fd729ba9f02af
Deco <gdecosmojr@gmail.com>
2022-04-17 19:38:41 -0400

    Added ethrnet led install

--- raspinet/ethled.sh
+++ raspinet/ethled.sh
@@ -1,34 +1,28 @@
-#!/bin/bash
+#!/usr/bin/bash
 # run this script every 60 seconds
-
 NICPATH="/sys/class/net"
 ETHUP="eth0/operstate"
-
 ETH0_STATE=$NICPATH/$ETHUP
 
-sudo usermod -aG gpio pi # needed to add permissions for gpio access 
+sudo usermod -aG gpio dev # needed to add permissions for gpio access 
 
 function ethled_on(){
-
-    gpio export 26 out
-    echo 1 > /sys/class/gpio/gpio26/value
-
+    sudo echo "26" >/sys/class/gpio/export
+	sudo echo "out" >/sys/class/gpio/gpio26/direction
+    sudo echo "1" > /sys/class/gpio/gpio26/value
 }
-
 function ethled_off(){
-
-    echo 0 > /sys/class/gpio/gpio26/value
-    gpio export 26 in
-    
+    sudo echo "0" > /sys/class/gpio/gpio26/value
+    sudo echo "26" >/sys/class/gpio/unexport   
 }
-
 isEthUp(){
 if [ `grep "" $ETH0_STATE` == "up" ] 
 then
-	ethled_on
+	echo "Turning on LED"
+	ethled_on 
 else
-	ethled_off
+	echo "Turning off LED"
+	ethled_off 
 fi
 }
-
-isEthUp
+isEthUp >/dev/null 2>&1
